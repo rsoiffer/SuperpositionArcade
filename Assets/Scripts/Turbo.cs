993455693f93f396a4f30fac31e0aa803def8049
rsoiffer <rsoiffer@gmail.com>
2023-10-01 18:22:05 -0700

    Implemented turbo button

---
+++ Assets/Scripts/Turbo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Turbo : MonoBehaviour
+{
+    public Button button;
+    public Level level;
+    public float turboSpeedMul = 10;
+    public float turboDuration = 2;
+
+    private bool activeNow;
+
+    private void Start()
+    {
+        button.onClick.AddListener(() =>
+        {
+            if (!activeNow)
+                StartCoroutine(RunTurbo());
+        });
+    }
+
+    private IEnumerator RunTurbo()
+    {
+        activeNow = true;
+        level.spawnRate *= turboSpeedMul;
+        yield return new WaitForSeconds(turboDuration);
+        level.spawnRate /= turboSpeedMul;
+        activeNow = false;
+    }
+}
